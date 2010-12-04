4b2749219381ee3c8b4e7a2b7272b8eb2c7bd756
Angelo Castriotta <angelocastriotta@kubepartners.com>
2010-12-04 11:02:10 +0100

    std::endl

---
+++ Coda/Source/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Coda
+//
+//  Created by Angelo Castriotta on 04/12/10.
+//  Copyright 2010 Kube Partners. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[]) {
+
+	// insert code here...
+	std::cout << "Hello, World!\n" << std::endl ;
+    return 0;
+}
+
