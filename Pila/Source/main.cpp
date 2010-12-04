29a89a7789dc60ba31a48aa8386f4a4699567fd7
Angelo Castriotta <angelocastriotta@kubepartners.com>
2010-12-04 10:57:58 +0100

    pila

---
+++ Pila/Source/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Pila
+//
+//  Created by Angelo Castriotta on 04/12/10.
+//  Copyright 2010 Kube Partners. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[]) {
+
+	// insert code here...
+	std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
